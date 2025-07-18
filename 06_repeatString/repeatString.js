0b8fb452a3a0e1c622c29281becdae98d8b9038d
Leslie Margaret <202473022+n00b-master69@users.noreply.github.com>
2025-07-18 20:22:57 +0800

    Pass test 6 and 7

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,16 @@
-const repeatString = function() {
+const repeatString = function(word, num) {
+    let repeatedWord = "";
+    for(let i = 1; i <= num; i++){
+        if(num > 0){
+            repeatedWord += word;
+        } 
+    }
 
+    if(num < 0){
+            repeatedWord = "ERROR";
+        }
+        
+    return repeatedWord;
 };
 
 // Do not edit below this line
--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function(values) {
+    let initialArray = [...values[0]];
+    let toBeRemoved = [...values.splice(0,1)];
+
+    for(let item of toBeRemoved){
+        if(initialArray.includes(item)){
+            let itemIndex = initialArray.findIndex(item);
+            initialArray.splice(itemIndex, 1);
+        }
+    }
+
+    return initialArray;
 };
 
 // Do not edit below this line

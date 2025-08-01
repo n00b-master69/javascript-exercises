c6057a2789cf594b63bf9681685bdbbbd1248c8c
Leslie Margaret <202473022+n00b-master69@users.noreply.github.com>
2025-08-01 18:55:23 +0800

    Pass test 12 & 13

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,10 @@
-const palindromes = function () {
-
+const palindromes = function (word) {
+    let trimmed = word.replaceAll(/[ ,.!?()-]/g, "").toLowerCase();
+    if(trimmed.split("").reverse().join("") === trimmed){
+        return true;
+    }else{
+        return false;
+    }
 };
 
 // Do not edit below this line

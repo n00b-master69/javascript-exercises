7499e2450aea0c77a135b3fbf8deae99edfda645
Leslie Margaret <202473022+n00b-master69@users.noreply.github.com>
2025-08-02 18:46:41 +0800

    Pass test 14, 15, and 16

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,19 @@
-const fibonacci = function() {
-
+const fibonacci = function(x) {
+    let a = 0;
+    let b = 1;
+    let arr = [a, b];
+    let total;
+    for(let i = 2; i <= x; i++){
+        total = a + b;
+        a = b;
+        b = total;
+        arr[i] = total;
+    }
+    if(+x < 0){
+        return "OOPS";
+    }else{
+        return arr[+x];
+    }
 };
 
 // Do not edit below this line
--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,4 +1,18 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    const date = new Date();
+    const currentYear = date.getFullYear();
+
+    people.map((person) => {
+        if(!person.yearOfDeath){
+            person.yearOfDeath = currentYear;
+        }
+        person.age = person.yearOfDeath - person.yearOfBirth;
+        return;
+    });
+
+    people.sort((previous, next) => next.age - previous.age );
+
+    return people[0];
 
 };
 
